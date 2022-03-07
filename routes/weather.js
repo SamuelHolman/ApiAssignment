4f015294847cafd40f539d5c904ce9144c930bf2
Samuel <sholman@gus.pittstate.edu>
2022-03-06 19:45:47 -0600

    Changed the parenthesis

--- routes/weather.js
+++ routes/weather.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const WeatherController = require("../controllers/WeatherController");
+const WeatherController = require('../controllers/WeatherController');
 
 /* GET users listing. */
-router.get('/', WeatherController.get_weather());
+router.get('/', WeatherController.get_weather);
 
 module.exports = router;
